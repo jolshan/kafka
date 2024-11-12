26c57fbed5b6488f254f7ab436db99af1d954ad5
jolshan <jolshan@confluent.io>
2024-11-12 13:02:58 -0800

    fix method

--- tests/kafkatest/services/kafka/kafka.py
+++ tests/kafkatest/services/kafka/kafka.py
@@ -896,9 +896,7 @@ class KafkaService(KafkaPathResolverMixin, JmxMixin, Service):
             if self.uses_transactions_v2:
                 cmd += " --feature transaction.version=2"
             else:
-                self.logger.info(get_version(node))
-                self.logger.info(get_version(node).supports_feature_command)
-                if get_version(node).supports_feature_command:
+                if get_version(node).supports_feature_command():
                     cmd += " --feature transaction.version=1"
             self.logger.info("Running log directory format command...\n%s" % cmd)
             node.account.ssh(cmd)
